793c43984f40c602a4f67f1b6402b7a407646011
Natalie <leaden.flo@gmail.com>
2021-06-28 19:15:20 +0300

    Lesson 1 1, 2, 3, 4, 5 tasks

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,6 @@
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
     let stringTruncated = str.slice(0, (maxlength - 1)) + '…';
-    console.log(stringTruncated);
     return stringTruncated;
   } else {
     return str;
