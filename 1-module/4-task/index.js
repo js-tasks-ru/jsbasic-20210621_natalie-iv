04a3c972c9d69623571da51125e5feecd2f9b88b
Natalie <leaden.flo@gmail.com>
2021-06-29 12:29:57 +0300

    Lesson 1 1, 2, 3, 4, 5 tasks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,5 +1,5 @@
 function checkSpam(str) {
-  return str.toLowerCase().match(/1xbet|xxx/) ? true : false;
+  return Boolean(str.toLowerCase().match(/1xbet|xxx/));
 }
 
 //todo: Я решила приводить значение к нижнему регистру, чтобы исключить чувствительность к регистру в проверке
