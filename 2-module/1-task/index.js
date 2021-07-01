5931d790ec1fa9a90cd65cd5c0b3e9cc21a548f9
Natalie <leaden.flo@gmail.com>
2021-07-01 20:17:38 +0300

    Lesson 2 1, 2, 3 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,6 +1,5 @@
 function sumSalary(salaries) {
   const numbers = Object.values(salaries).filter(number => typeof (number) === 'number' && isFinite(number));
-  console.log(numbers, 'numbers');
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
     sum = sum + numbers[i];
