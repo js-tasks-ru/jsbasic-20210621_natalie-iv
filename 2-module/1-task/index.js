bb4466bc9d682cec464ed3cd4448e2e93a3f9837
Natalie <leaden.flo@gmail.com>
2021-07-04 21:54:58 +0300

    Lesson 2 1, 2, 3 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,6 @@
 function sumSalary(salaries) {
   const numbers = Object.values(salaries).filter(number => typeof (number) === 'number' && isFinite(number));
   return numbers.length > 0 ? numbers.reduce((a, b) => {
-    console.log(a, b);
     return a + b;
   }) : 0;
 }
