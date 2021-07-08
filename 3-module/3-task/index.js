4be83592f4c317b9169d0ab344baee4ff40cd232
Natalie <leaden.flo@gmail.com>
2021-07-08 14:18:06 +0300

    commit

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,11 +1,5 @@
 function camelize(str) {
-  if (str.charAt(0) === '-') {
-    return str.split('-').map(item => item.charAt(0).toUpperCase() + item.slice(1)).join('');
-  } else {
-    return str.split('-').map((item, index) => index === 0 ? item : item.charAt(0).toUpperCase() + item.slice(1)).join('');
-  }
+  return str.split('-')
+            .map((item, index) => index === 0 ? item : item.charAt(0).toUpperCase() + item.slice(1))
+            .join('');
 }
-// todo: Изменила на решение с index === 0, я просто не знаю как еще это решить. Чтобы результат был такой как на примерах:
-// camelize('background-color') == 'backgroundColor';
-// camelize('list-style-image') == 'listStyleImage';
-// camelize('-webkit-transition') == 'WebkitTransition';
