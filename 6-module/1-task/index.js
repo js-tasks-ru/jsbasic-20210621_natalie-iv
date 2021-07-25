b606251c5488233cdcd3d72f3d912806bf47006a
Natalie <leaden.flo@gmail.com>
2021-07-25 23:48:32 +0300

    Module 6 Tasks 1, 2, 3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,48 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    this.deleteTableRow();
+  }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.dataset.component = 'usersTable';
+    this.elem.innerHTML = this.createTable({
+      items: this.rows
+    });
+  }
+
+  createTable ({items}) {
+    return `
+        <thead>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Salary</th>
+            <th>City</th>
+            <th></th>
+        </thead>
+        <tbody>
+        ${items.map((item) => `<tr>
+                                <td>${item.name}</td>
+                                <td>${item.age}</td>
+                                <td>${item.salary}</td>
+                                <td>${item.city}</td>
+                                <td><button>X</button></td>
+                             </tr>`)
+      .join('')}
+        </tbody>
+  `;
+  }
+
+  deleteTableRow () {
+    this.elem.onclick = ({target}) => {
+      let button = target.closest("button");
+      if (button) {
+        let parentButton = target.parentNode;
+        let currentRow = parentButton.parentNode;
+        currentRow.remove();
+      }
+    };
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,89 @@
-import createElement from '../../assets/lib/create-element.js';
-
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentSlide = 0;
+    this.createSlider();
+    this.carouselMechanics();
+  }
+
+  createSlider () {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('carousel');
+    this.elem.innerHTML = `
+          <div class="carousel__arrow carousel__arrow_right">
+            <img src="./../../assets/images/icons/angle-icon.svg" alt="icon" />
+          </div>
+          <div class="carousel__arrow carousel__arrow_left">
+            <img src="./../../assets/images/icons/angle-left-icon.svg" alt="icon" />
+          </div>
+          <div class="carousel__inner"></div>
+    `;
+    let items = this.slides;
+    let carouselInner = this.elem.getElementsByClassName('carousel__inner')[0];
+    for (let i = 0; i < items.length; i++) {
+      let card = document.createElement('div');
+      card.classList.add('carousel__slide');
+      card.setAttribute('data-id', `${items[i].id}`);
+      card.innerHTML = this.createSlideCard({
+        image: items[i].image,
+        price: items[i].price,
+        name: items[i].name
+      });
+      carouselInner.append(card);
+      let button = this.elem.getElementsByClassName('carousel__button')[i];
+      button.addEventListener('click', () => {
+        this.addCard(items[i].id);
+      });
+    }
+    this.updateSlider();
+  }
+
+  createSlideCard({image, price, name}) {
+    return `
+        <img src="./../../assets/images/carousel/${image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+            <span class="carousel__price">€${price.toFixed(2)}</span>
+            <div class="carousel__title">${name}</div>
+            <button type="button" class="carousel__button">
+                <img src="./../../assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+        </div>
+  `;
+  }
+
+  addCard(id) {
+    let event = new CustomEvent("product-add", {
+      detail: id,
+      bubbles: true
+    });
+    document.body.dispatchEvent(event);
+  }
+
+  updateSlider() {
+    let offset = -this.elem.offsetWidth * this.currentSlide;
+    this.elem.querySelector(`.carousel__inner`).style.transform = `translateX(${offset}px)`;
+
+    if (this.currentSlide === this.slides.length - 1) {
+      this.elem.querySelector(`.carousel__arrow_right`).style.display = 'none';
+    } else {
+      this.elem.querySelector(`.carousel__arrow_right`).style.display = '';
+    }
+
+    if (this.currentSlide === 0) {
+      this.elem.querySelector(`.carousel__arrow_left`).style.display = 'none';
+    } else {
+      this.elem.querySelector(`.carousel__arrow_left`).style.display = '';
+    }
+  }
+
+  carouselMechanics() {
+    this.elem.querySelector(`.carousel__arrow_right`).onclick = () => {
+      this.currentSlide++;
+      this.updateSlider();
+    };
+    this.elem.querySelector(`.carousel__arrow_left`).onclick = () => {
+      this.currentSlide--;
+      this.updateSlider();
+    };
   }
 }
